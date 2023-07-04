be92794c2890f49c5266ef453696de208075ff40
Pels <pelumisunday@gmail.com>
2023-07-04 10:24:19 +0100

    feat: create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const toggle = ref(false)
+</script>
+
+
+<template>
+  <header class="flex justify-between items-center p-4 shadow-sm">
+    <h3 class="font-bold">Where in the world?</h3>
+    <div class="flex items-center gap-2">
+      <img src="@/assets/icons/moon-outline.svg" alt="icon-moon" class="w-4 cursor-pointer" />
+      <p>{{ toggle ? 'Dark Mode' : 'Light Mode' }}</p>
+    </div>
+  </header>
+</template>

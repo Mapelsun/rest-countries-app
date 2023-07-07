03fc92b0293fa1dc6ec301692b21752d6e6947d8
Pels <pelumisunday@gmail.com>
2023-07-07 14:13:30 +0100

    feat: add light and dark mode functionalities

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,36 @@
 <script setup>
-import { ref } from 'vue'
-const toggle = ref(false)
+import { ref, onMounted, watch } from 'vue'
+const modeVal = ref(localStorage.getItem('theme') || 'light')
+
+const checkDarkMode = () => {
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.theme = 'dark'
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.theme = 'light'
+  }
+}
+
+const toggleDarkMode = () => {
+  if (modeVal.value === 'dark') {
+    modeVal.value = 'light'
+  } else if (modeVal.value === 'light') {
+    modeVal.value = 'dark'
+  }
+}
+
+watch(modeVal, (newVal) => {
+  if (newVal === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.theme = 'light'
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.theme = 'dark'
+  }
+}, {immediate: true})
+
+onMounted(() => checkDarkMode())
 </script>
 
 
@@ -8,8 +38,8 @@ const toggle = ref(false)
   <header class="flex justify-between items-center p-4 shadow-sm">
     <h3 class="font-bold">Where in the world?</h3>
     <div class="flex items-center gap-2">
-      <img src="@/assets/icons/moon-outline.svg" alt="icon-moon" class="w-4 cursor-pointer" />
-      <p>{{ toggle ? 'Dark Mode' : 'Light Mode' }}</p>
+      <img src="@/assets/icons/moon-outline.svg" alt="icon-moon" class="w-4 cursor-pointer" @click="toggleDarkMode" />
+      <p>{{ modeVal === 'dark' ? 'Dark Mode' : 'Light Mode' }}</p>
     </div>
   </header>
 </template>

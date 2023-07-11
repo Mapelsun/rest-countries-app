0f0172ab9aae6bef3730f6caf05c17cecc0db143
Pels <pelumisunday@gmail.com>
2023-07-11 18:33:37 +0100

    update: pass countries props to country card

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  countries: Array,
+})
+</script>
+
+<template>
+  <section>
+    <div v-for="{ flag, flagAlt, name, population, region, capital, area } in countries" :key="area">
+      <img :src="flag" :alt="flagAlt" />
+      <div>
+        <h4>{{ name }}</h4>
+        <p>Population: {{ population }}</p>
+        <p>Region: {{ region }}</p>
+        <p>Capital: {{ capital }}</p>
+      </div>
+    </div>
+  </section>
+</template>

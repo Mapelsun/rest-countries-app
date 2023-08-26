480cd9a9a5ade79d2e5843b4fce2df62e5901b78
Pels <pelumisunday@gmail.com>
2023-08-26 20:33:37 +0100

    style: update bottom padding for texts

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -8,7 +8,7 @@ const props = defineProps({
   <div v-for="{ flag, flagAlt, name, population, region, capital } in countries" :key="name"
     class="flex flex-col rounded shadow-lg overflow-hidden">
     <img :src="flag" :alt="flagAlt" class="h-48 w-full object-cover" />
-    <div class="p-6">
+    <div class="px-6 pt-6 pb-10">
       <h4 class="font-bold mb-3 text-xl">{{ name }}</h4>
       <p class="text-base"><span class="font-semibold">Population:</span><span>&nbsp;{{ population }}</span></p>
       <p class="text-base"><span class="font-semibold">Region:</span><span>&nbsp;{{ region }}</span></p>

2a28653add98dfcd060dd829dc49e23b54ce2b2b
Pels <pelumisunday@gmail.com>
2023-08-26 20:30:28 +0100

    style: add custom responsive grid col style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // responsive column grid
+        'auto': 'repeat(auto-fit, minmax(17rem, 1fr))',
+      }
+    },
   },
   plugins: [],
 }

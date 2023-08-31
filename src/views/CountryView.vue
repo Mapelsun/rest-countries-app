ea6fdf152b2e16d3b0a8cc90aeca2dc81b2d11c1
Pels <pelumisunday@gmail.com>
2023-08-31 17:57:49 +0100

    feat: create country view page

---
+++ src/views/CountryView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+</script>
+<template>
+  <div>
+    {{ router }} - {{ route }}
+    </div>
+</template>

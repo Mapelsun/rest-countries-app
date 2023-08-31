ab81e512071bda4e3beaa4f55ea308612b458b02
Pels <pelumisunday@gmail.com>
2023-08-31 17:22:58 +0100

    style: make background white for search area

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -4,7 +4,7 @@ const queryTxt = ref('')
 
 </script>
 <template>
-  <div class="flex items-center gap-5 px-8 py-4 rounded-md shadow-md">
+  <div class="flex items-center gap-5 px-8 py-4 rounded-md shadow-md bg-white">
     <img src="@/assets/icons/search-outline.svg" alt="search icon" class="w-5" />
     <input v-model="queryTxt" type="text" placeholder="Search for a country..."
       class="outline-none w-full bg-inherit text-sm">

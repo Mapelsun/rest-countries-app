2d2efdf5a00c6cfea7749cc3fb77c6c5a447d5a2
Pels <pelumisunday@gmail.com>
2023-09-21 17:08:24 +0100

    refactor: move header to app comp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
 <script setup>
-import Header from '@/components/Header.vue'
 import SearchFilter from '@/components/SearchFilter.vue'
 import CountryList from '@/components/CountryList.vue'
 </script>
 <template>
-  <main>
-    <Header />
-    <SearchFilter />
-    <CountryList />
-  </main>
+  <SearchFilter />
+  <CountryList />
 </template>

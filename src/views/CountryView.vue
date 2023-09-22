7f9f7205e1d9709a7b199bbd9de5c8752d3bdfc9
Pels <pelumisunday@gmail.com>
2023-09-22 11:34:47 +0100

    update: replace dummy data with api data

--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -1,9 +1,52 @@
 <script setup>
+import api from '@/utils/api-services.js';
+import { formatNumberWithCommas, getCapital } from '@/utils/helpers.js'
+
+import { ref, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
+
 import { ArrowLongLeftIcon } from '@heroicons/vue/24/solid'
 import Container from '@/components/Container.vue'
+
 const router = useRouter()
 const route = useRoute()
+
+const loading = ref(true)
+const countryInfo = ref({})
+
+const getCountryInfo = async () => {
+  try {
+    loading.value = true
+    const { data } = await api.getCountry(route.params.id)
+    countryInfo.value = data[0]
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false
+  }
+}
+
+const formatLanguages = (country) => {
+  return Object.values(country.languages).join(', ')
+}
+
+const formatCurrencies = (country) => {
+  const currencies = country.currencies
+  const result = [];
+  for (let key in currencies) {
+    if (currencies.hasOwnProperty(key)) {
+      let nestedObj = currencies[key]
+      if (nestedObj.hasOwnProperty("name")) {
+        result.push(nestedObj.name)
+      }
+    }
+  }
+  return result.join(', ')
+}
+
+onMounted(() => {
+  getCountryInfo()
+})
 </script>
 <template>
   <Container>
@@ -11,23 +54,31 @@ const route = useRoute()
       @click="router.back()">
       <ArrowLongLeftIcon class="w-4" /><span>Back</span>
     </button>
-    <div class="grid sm:grid-cols-2 sm:items-center">
+    <p v-if="loading">Loading...</p>
+    <div v-else class="grid sm:grid-cols-2 sm:items-center">
       <img class="w-full max-w-2xl h-60 md:h-full mb-6 sm:mb-0 bg-red-500" src="" alt="">
       <div class="sm:pl-5 sm:mt-5 md:pl-10 lg:pl-20">
-        <h4 class="font-bold mb-4 text-xl">Belgium</h4>
+        <h2 class="text-2xl font-bold mb-4">{{ countryInfo.name.common }}</h2>
         <div class="md:flex md:gap-10 lg:gap-16">
           <div class="mb-8">
-            <p class="text-cus"><span class="font-semibold">Native Name:</span><span>&nbsp;Belgie</span></p>
-            <p class="text-cus"><span class="font-semibold">Population:</span><span>&nbsp;11,319,511</span>
+            <p class="text-cus"><span class="font-semibold">Native Name:</span><span>&nbsp;{{ countryInfo.name.official
+            }}</span></p>
+            <p class="text-cus"><span class="font-semibold">Population:</span><span>&nbsp;{{
+              formatNumberWithCommas(countryInfo.population) }}</span>
             </p>
-            <p class="text-cus"><span class="font-semibold">Region:</span><span>&nbsp;Europe</span></p>
-            <p class="text-cus"><span class="font-semibold">Sub Region:</span><span>&nbsp;Western Europe</span></p>
-            <p class="text-cus"><span class="font-semibold">Capital:</span><span>&nbsp;Brussels</span></p>
+            <p class="text-cus"><span class="font-semibold">Region:</span><span>&nbsp;{{ countryInfo.region }}</span></p>
+            <p class="text-cus"><span class="font-semibold">Sub Region:</span><span>&nbsp;{{ countryInfo.subregion
+            }}</span></p>
+            <p class="text-cus"><span class="font-semibold">Capital:</span><span>&nbsp;{{ getCapital(countryInfo)
+            }}</span></p>
           </div>
           <div class="mb-8">
-            <p class="text-cus"><span class="font-semibold">Top Level Domain:</span><span>&nbsp;.be</span></p>
-            <p class="text-cus"><span class="font-semibold">Currencies:</span><span>&nbsp;Euro</span></p>
-            <p class="text-cus"><span class="font-semibold">Languages:</span><span>&nbsp;Dutch, French, German</span></p>
+            <p class="text-cus"><span class="font-semibold">Top Level Domain:</span><span>&nbsp;{{ countryInfo.tld[0]
+            }}</span></p>
+            <p class="text-cus"><span class="font-semibold">Currencies:</span><span>&nbsp;{{ formatCurrencies(countryInfo)
+            }}</span></p>
+            <p class="text-cus"><span class="font-semibold">Languages:</span><span>&nbsp;{{ formatLanguages(countryInfo)
+            }}</span></p>
           </div>
         </div>
         <div>

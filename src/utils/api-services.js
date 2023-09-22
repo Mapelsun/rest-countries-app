8262f6558ab7cee1db4a34dac995176776913eed
Pels <pelumisunday@gmail.com>
2023-09-22 11:33:37 +0100

    feat: add endpoint to fetch single country

--- src/utils/api-services.js
+++ src/utils/api-services.js
@@ -3,5 +3,8 @@ import axios from 'axios'
 export default {
   getAllCountries: () => {
     return axios.get('https://restcountries.com/v3.1/all')
+  },
+  getCountry: (id) => {
+    return axios.get(`https://restcountries.com/v3.1/alpha/${id}`)
   }
 }

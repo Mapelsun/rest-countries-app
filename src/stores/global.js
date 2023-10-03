13182d4285afcaebb4a39d710a88000592b0f269
Pels <pelumisunday@gmail.com>
2023-10-03 19:07:01 +0100

    feat: add action to get border countries name

--- src/stores/global.js
+++ src/stores/global.js
@@ -1,10 +1,22 @@
 import { defineStore } from 'pinia'
 
 export const useGlobalStore = defineStore('global', {
-  state: () => ({ counties: [] }),
+  state: () => ({ countries: [] }),
   actions: {
     setAllCountries(val) {
       this.countries = val
     },
+    getBorderCountries(borderCountries) {
+      if (!borderCountries) return 'N/A'
+      let filteredBorderCountries = []
+      for (let i = 0; i < borderCountries.length; i++) {
+        for (let j = 0; j < this.countries.length; j++) {
+          if (borderCountries[i] === this.countries[j].cca3) {
+            filteredBorderCountries.push(this.countries[j].name.common)
+          }
+        }
+      }
+      return filteredBorderCountries
+    }
   },
 })

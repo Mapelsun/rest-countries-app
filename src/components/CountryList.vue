14d0f31c2ce717c0390f03d44b8dfe9f0dac0f78
Pels <pelumisunday@gmail.com>
2023-10-03 19:31:27 +0100

    perf: ensure /all is only called once within

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Container from '@/components/Container.vue'
 import CountryCard from '@/components/CountryCard.vue'
-import api from '@/utils/api-services.js';
+import api from '@/utils/api-services.js'
 import { getCapital } from '@/utils/helpers.js'
 import { ref, onMounted } from 'vue'
 import { useGlobalStore } from '@/stores/global'
@@ -10,21 +10,35 @@ const defaultCountries = ref([])
 
 const getCountries = async () => {
   try {
-    const { data } = await api.getAllCountries()
-    global.setAllCountries(data)
-
-    defaultCountries.value = data.slice(0, 8).map(country => {
-      return {
-        flag: country.flags.svg,
-        flagAlt: country.flags.alt || '',
-        name: country.name.common,
-        population: country.population,
-        region: country.region,
-        code: country.cca2,
-        capital: getCapital(country)
-      }
-    })
-
+    let data
+    if (global.countries.length > 0) {
+      data = global.countries
+      defaultCountries.value = data.slice(0, 8).map((country) => {
+        return {
+          flag: country.flags.svg,
+          flagAlt: country.flags.alt || '',
+          name: country.name.common,
+          population: country.population,
+          region: country.region,
+          code: country.cca2,
+          capital: getCapital(country),
+        }
+      })
+    } else {
+      const { data } = await api.getAllCountries()
+      global.setAllCountries(data)
+      defaultCountries.value = data.slice(0, 8).map((country) => {
+        return {
+          flag: country.flags.svg,
+          flagAlt: country.flags.alt || '',
+          name: country.name.common,
+          population: country.population,
+          region: country.region,
+          code: country.cca2,
+          capital: getCapital(country),
+        }
+      })
+    }
   } catch (error) {
     console.log(error)
   }
@@ -35,11 +49,15 @@ onMounted(() => {
 })
 </script>
 
-
 <template>
   <container>
     <section class="flex flex-col gap-10 sm:grid sm:grid-cols-auto">
-      <CountryCard :country="country" v-for="country in defaultCountries" :key="country.name" />
+      <CountryCard
+        :country="country"
+        v-for="country in defaultCountries"
+        :key="country.name"
+      />
     </section>
   </container>
 </template>
+

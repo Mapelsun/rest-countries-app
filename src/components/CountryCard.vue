4c8da4854e9654e4a6a11f02d5f08d331c775f86
Pels <pelumisunday@gmail.com>
2023-10-05 18:15:12 +0100

    perf: optimise data unidirectional flow

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,23 +1,42 @@
 <script setup>
 import { useRouter } from 'vue-router'
-import { convertToLowerCase, formatNumberWithCommas } from '@/utils/helpers.js'
+import {
+  convertToLowerCase,
+  formatNumberWithCommas,
+  getCapital,
+} from '@/utils/helpers.js'
 const router = useRouter()
 
 const props = defineProps({
-  country: Object
+  country: Object,
 })
 </script>
 
 <template>
-  <div @click="router.push(`/country/${convertToLowerCase(country.code)}`)"
-    class="flex flex-col rounded shadow-lg hover:shadow-2xl transition overflow-hidden cursor-pointer">
-    <img :src="country.flag" :alt="country.flagAlt" class="h-48 w-full object-cover" />
+  <div
+    @click="router.push(`/country/${convertToLowerCase(country.cca2)}`)"
+    class="flex flex-col rounded shadow-lg hover:shadow-2xl transition overflow-hidden cursor-pointer"
+  >
+    <img
+      :src="country.flags.svg"
+      :alt="country.flags.alt || ''"
+      class="h-48 w-full object-cover"
+    />
     <div class="px-6 pt-6 pb-10">
-      <h4 class="font-bold mb-3 text-xl">{{ country.name }}</h4>
-      <p class="text-sm"><span class="font-semibold">Population:</span><span>&nbsp;{{
-        formatNumberWithCommas(country.population) }}</span></p>
-      <p class="text-sm"><span class="font-semibold">Region:</span><span>&nbsp;{{ country.region }}</span></p>
-      <p class="text-sm"><span class="font-semibold">Capital:</span><span>&nbsp;{{ country.capital }}</span></p>
+      <h4 class="font-bold mb-3 text-xl">{{ country.name.common }}</h4>
+      <p class="text-sm">
+        <span class="font-semibold">Population:</span
+        ><span>&nbsp;{{ formatNumberWithCommas(country.population) }}</span>
+      </p>
+      <p class="text-sm">
+        <span class="font-semibold">Region:</span
+        ><span>&nbsp;{{ country.region }}</span>
+      </p>
+      <p class="text-sm">
+        <span class="font-semibold">Capital:</span
+        ><span>&nbsp;{{  getCapital(country) }}</span>
+      </p>
     </div>
   </div>
 </template>
+
--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -2,36 +2,21 @@
 import Container from '@/components/Container.vue'
 import CountryCard from '@/components/CountryCard.vue'
 import api from '@/utils/api-services.js'
-import { getCapital } from '@/utils/helpers.js'
 import { ref, onMounted } from 'vue'
 import { useGlobalStore } from '@/stores/global'
 const global = useGlobalStore()
 const defaultCountries = ref([])
-
-const setLandingCountries = (data) => {
-  defaultCountries.value = data.map((country) => {
-    return {
-      flag: country.flags.svg,
-      flagAlt: country.flags.alt || '',
-      name: country.name.common,
-      population: country.population,
-      region: country.region,
-      code: country.cca2,
-      capital: getCapital(country),
-    }
-  })
-}
 
 const getCountries = async () => {
   try {
     let data
     if (global.countries.length > 0) {
       data = global.countries
-      setLandingCountries(data)
+      defaultCountries.value = data
     } else {
       const { data } = await api.getAllCountries()
       global.setAllCountries(data)
-      setLandingCountries(data)
+      defaultCountries.value = data
     }
   } catch (error) {
     console.log(error)
@@ -49,7 +34,7 @@ onMounted(() => {
       <CountryCard
         :country="country"
         v-for="country in defaultCountries"
-        :key="country.name"
+        :key="country.cca2"
       />
     </section>
   </container>

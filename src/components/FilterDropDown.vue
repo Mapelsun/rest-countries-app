21f26c24694df6f015150b72cec6bfb6bcca5e8f
Pels <pelumisunday@gmail.com>
2023-10-10 20:29:42 +0100

    update: get regions from store

--- src/components/FilterDropDown.vue
+++ src/components/FilterDropDown.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ref } from 'vue'
-import {ChevronDownIcon } from '@heroicons/vue/24/solid'
+import { ChevronDownIcon } from '@heroicons/vue/24/solid'
 const selected = ref('')
 const showDropdown = ref(false)
-const regions = ref(['Africa', 'America', 'Asia', 'Europe', 'Oceania'])
+import { useGlobalStore } from '@/stores/global'
+const global = useGlobalStore()
+
 const toggleOptions = () => {
   showDropdown.value = !showDropdown.value
 }
+
 const setSelected = (region) => {
   selected.value = region
   showDropdown.value = false
@@ -15,12 +18,25 @@ const setSelected = (region) => {
 
 <template>
   <div class="relative">
-    <div class="flex justify-between gap-5 px-5 py-4 rounded-md shadow-md w-60 bg-white" @click="toggleOptions">
-      <span class="text-sm">Filter by Region</span>
+    <div
+      class="flex justify-between gap-5 px-5 py-4 rounded-md shadow-md w-60 bg-white"
+      @click="toggleOptions"
+    >
+      <span class="text-sm text-gray-400">Filter by Region</span>
       <ChevronDownIcon class="w-4 text-gray-500" />
     </div>
-    <div class="absolute top-16 flex flex-col px-5 py-4 rounded-md shadow-md w-60 bg-white transition" v-if="showDropdown">
-      <span v-for="region in regions" :key="region" class="text-base cursor-pointer" @click="setSelected(region)">{{ region }}</span>
+    <div
+      class="absolute top-16 flex flex-col px-5 py-4 rounded-md shadow-md w-60 bg-white transition"
+      v-if="showDropdown"
+    >
+      <span
+        v-for="region in global.regions"
+        :key="region"
+        class="text-base text-gray-400 cursor-pointer"
+        @click="setSelected(region)"
+        >{{ region }}</span
+      >
     </div>
   </div>
 </template>
+

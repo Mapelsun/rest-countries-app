62705446b7e7cee197b436219a80a0f71e3c848a
Pels <pelumisunday@gmail.com>
2023-10-10 20:28:30 +0100

    perf: don't show filter and list till data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <script setup>
 import SearchFilter from '@/components/SearchFilter.vue'
 import CountryList from '@/components/CountryList.vue'
+import { useGlobalStore } from '@/stores/global'
+const global = useGlobalStore()
 </script>
 <template>
-  <SearchFilter />
-  <CountryList />
+  <div>
+    <p class="max-w-screen-xl mx-auto px-4" v-if="global.loading">Loading...</p>
+    <div v-else>
+      <SearchFilter />
+      <CountryList />
+    </div>
+  </div>
 </template>
+

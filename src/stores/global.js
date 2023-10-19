f553d3a67c4c6a8780bec082649eb19e456b7fdb
Pels <pelumisunday@gmail.com>
2023-10-19 11:37:47 +0100

    feat: add 'All' as an option to displayed regions

--- src/stores/global.js
+++ src/stores/global.js
@@ -28,16 +28,26 @@ export const useGlobalStore = defineStore('global', {
         .map((country) => ({ name: country.name.common, cca2: country.cca2 }))
     },
     setRegions() {
-      const regions = []
+      const regions = ['All']
       this.countries.forEach((obj) => {
         if (!regions.includes(obj.region)) {
           regions.push(obj.region)
         }
       })
-      this.regions = regions.sort()
+      this.regions = regions.sort(function (a, b) {
+        if (a === 'All') {
+          return -1
+        } else if (b === 'All') {
+          return 1
+        } else {
+          return a.localeCompare(b)
+        }
+      })
     },
     filterRegions(region) {
-      this.countriesByRegion = this.countries.filter((country) => country.region === region)
+      this.countriesByRegion = this.countries.filter(
+        (country) => country.region === region
+      )
     },
   },
 })

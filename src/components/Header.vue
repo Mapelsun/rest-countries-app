c487c283d55ab10c27331b438133ed9d5800c033
Pels <pelumisunday@gmail.com>
2023-10-24 09:01:33 +0100

    refactor: move switch theme button to comp

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,24 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
-import { MoonIcon } from '@heroicons/vue/24/solid'
-const modeVal = ref(localStorage.getItem('theme') || 'light')
+import { useRouter } from 'vue-router'
 import Container from '@/components/Container.vue'
+import ThemeButton from '@/components/ThemeButton.vue'
 
-const checkDarkMode = () => {
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    localStorage.theme = 'dark'
-  } else {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    localStorage.theme = 'light'
-  }
-}
-
-const toggleDarkMode = () => {
-  if (modeVal.value === 'dark') {
-    modeVal.value = 'light'
-  } else if (modeVal.value === 'light') {
-    modeVal.value = 'dark'
-  }
-}
-
-watch(modeVal, (newVal) => {
-  if (newVal === 'dark') {
-    document.documentElement.setAttribute('data-theme', 'light')
-    localStorage.theme = 'light'
-  } else {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    localStorage.theme = 'dark'
-  }
-}, { immediate: true })
-
-onMounted(() => checkDarkMode())
+const router = useRouter()
 </script>
 
-
 <template>
-  <header class="shadow-sm sticky top-0"
-    :class="modeVal === 'dark' ? 'bg-[--color-background-light]' : 'bg-[--color-background-dark]'">
+  <header class="shadow-sm sticky top-0 bg-[--accent-color]">
     <container>
       <div class="flex justify-between items-center">
-        <h3 class="font-bold text-base">Where in the world?</h3>
-        <div class="flex items-center gap-2">
-          <MoonIcon class="w-4 cursor-pointer" @click="toggleDarkMode" />
-          <p>{{ modeVal === 'dark' ? 'Dark Mode' : 'Light Mode' }}</p>
-        </div>
+        <h3
+          class="font-bold text-base cursor-pointer"
+          @click="router.push('/')"
+        >
+          Where in the world?
+        </h3>
+        <ThemeButton />
       </div>
     </container>
   </header>
 </template>
+
